7f311adc41a0331fd97f9a2e187cbcb9a2e8e6d0
Rizky subekty <rizky.subekty@iconpln.co.id>
2025-11-26 18:56:01 +0700

    first commit

---
+++ themes/frontend/assets/js/color-settings.js
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>404 | Halaman Tidak Ditemukan</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="SmartCMS Powered by Sevenlight" name="description" />
+    <meta content="Themesbrand" name="sevenlight" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#5b73e8">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="shortcut icon" href="https://bogorwebsite.com/themes/smart/images/favicon.png">
+    <link href="https://bogorwebsite.com/themes/smart/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="https://bogorwebsite.com/themes/smart/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://bogorwebsite.com/themes/smart/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="authentication-bg">
+    <div class="my-5 pt-sm-5">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <div>
+                            <div class="row justify-content-center">
+                                <div class="col-sm-4">
+                                    <div class="error-img">
+                                        <img src="https://bogorwebsite.com/themes/smart/images/404-error.png" alt="" class="img-fluid mx-auto d-block">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="text-uppercase mt-4">MAAF, HALAMAN TIDAK DITEMUKAN</h4>
+                        <p class="text-muted">Sepertinya kami tidak dapat menemukan halaman yang Anda cari :)</p>
+                        <div class="mt-5">
+                            <a class="btn btn-danger waves-effect waves-light" href="javascript:window.history.back(-1);"><i class="fas fa-chevron-circle-left"></i> Kembali</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- JAVASCRIPT -->
+    <script src="https://bogorwebsite.com/themes/smart/libs/jquery/jquery.min.js"></script>
+    <script src="https://bogorwebsite.com/themes/smart/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
